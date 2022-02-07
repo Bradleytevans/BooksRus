8f0039a7a20daff4926f4b7d6ad24c5f9131e34f
bradleytevans <bradleyt.evans@gmail.com>
2022-02-07 11:16:44 -0500

    deployment

--- server/server.js
+++ server/server.js
@@ -1,17 +1,4 @@
 const express = require('express');
-<<<<<<< HEAD
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
-
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// if we're in production, serve client/build as static assets
-=======
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 
@@ -33,17 +20,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Serve up static assets
->>>>>>> 14ebae757a9cb07ce7eb3cb79a7894c25854cad9
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-<<<<<<< HEAD
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-=======
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
@@ -53,5 +33,4 @@ db.once('open', () => {
     console.log(`API server running on port ${PORT}!`);
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
->>>>>>> 14ebae757a9cb07ce7eb3cb79a7894c25854cad9
 });
